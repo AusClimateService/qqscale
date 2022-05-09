4562653009616a9d4a7946f9c8ae7177fa4583b2
Damien Irving <irving.damien@gmail.com>
2022-05-09 17:10:25 +1000

    More workflow

--- apply_adjustment.py
+++ apply_adjustment.py
@@ -1,46 +1,86 @@
 """Command line program for applying QQ-scaling adjustment factors."""
 import pdb
 import argparse
+import time
 
 import numpy as np
 import xarray as xr
+import xclim as xc
 from xclim import sdba
 import xesmf as xe
 import cmdline_provenance as cmdprov
 
 
+def check_units(da_obs, qm, obs_units, aj_units, output_units):
+    """Check units and convert if necessary."""
+
+    if obs_units:
+        da_obs.attrs['units'] = args.obs_units
+    if aj_units:
+        qm.ds['af'].attrs['units'] = aj_units
+        qm.ds['hist_q'].attrs['units'] = aj_units
+
+    if output_units:
+        if da_obs.attrs['units'] != output_units:
+            da_obs = xc.units.convert_units_to(da_obs, output_units)
+        if qm.ds['af'].attrs['units'] != output_units:
+            qm.ds['af'] = xc.units.convert_units_to(qm.ds['af'], output_units)
+        if qm.ds['hist_q'].attrs['units'] != output_units:
+            qm.ds['hist_q'] = xc.units.convert_units_to(qm.ds['hist_q'], output_units)
+    
+    return da_obs, qm
+
+
 def main(args):
     """Run the program."""
 
+    timer_start = time.perf_counter()
+
     ds_obs = xr.open_mfdataset(args.obs_files)
-    start_obs, end_obs = args.obs_time_bounds
-    ds_obs = da_obs.sel({'time': slice(start_obs, end_obs)})
+    start_obs, end_obs = args.time_bounds
+    ds_obs = ds_obs.sel({'time': slice(start_obs, end_obs)})
     ds_obs = ds_obs.chunk({'time': -1})
-    # TODO: Check units
 
     ds_adjust = xr.open_dataset(args.adjustment_file)
-    qm = sdba.QuantileDeltaMapping.from_dataset(ds)
+    qm = sdba.QuantileDeltaMapping.from_dataset(ds_adjust)
     regridder = xe.Regridder(qm.ds, ds_obs, "bilinear")
     qm.ds = regridder(qm.ds)
     qm.ds = qm.ds.compute()
 
-    if args.lon_selection:
-        start_index, end_index = args.lon_selection
-        ds_obs = ds_obs.isel({'lon': slice(start_index, end_index)})
-        qm.ds = qm.ds.isel({'lon': slice(start_index, end_index)}) 
+    chunk_num, total_chunks = args.lon_chunking
+    lon_chunks = np.array_split(qm.ds['lon'], total_chunks)
+    lon_selection = lon_chunks[chunk_num - 1] 
+    ds_obs = ds_obs.sel({'lon': lon_selection})
+    qm.ds = qm.ds.sel({'lon': lon_selection}) 
+    da_obs = ds_obs[args.variable]
 
+    da_obs, qm = check_units(
+        da_obs,
+        qm,
+        args.obs_units,
+        args.adjustment_units,
+        args.output_units
+    )
     qq_obs = qm.adjust(
-        ds_obs[args.variable],
+        da_obs,
         extrapolation="constant",
         interp="linear"
     )
+    
     qq_obs = qq_obs.rename(args.variable)
     qq_obs = qq_obs.transpose('time', 'lat', 'lon')
+    new_start_date = ds_adjust.attrs['future_period_start'] 
+    time_adjustment = np.datetime64(new_start_date) - qq_obs['time'][0]
+    qq_obs['time'] = qq_obs['time'] + time_adjustment
 
     qq_obs.attrs['history'] = cmdprov.new_log(
-        infile_logs={args.adjustment_file: qm.ds.attrs['history']},
+        infile_logs={args.adjustment_file: ds_adjust.attrs['history']},
     )
     qq_obs.to_netcdf(args.output_file)
+
+    timer_end = time.perf_counter()
+    total_time = (timer_end - timer_start) / 60.0
+    print(f'Adjustment duration = {total_time:0.4f} minutes')
 
 
 if __name__ == '__main__':
@@ -50,24 +90,21 @@ if __name__ == '__main__':
         formatter_class=argparse.RawDescriptionHelpFormatter
     )
                           
-    parser.add_argument(
-        "obs_files", type=str, nargs='*', help="observation data"
-    )           
-    parser.add_argument(
-        "variable", type=str, help="model variable to process"
-    )
-    parser.add_argument(
-        "adjustment_file", type=str, help="adjustment factor file"
-    )
+    parser.add_argument("obs_files", type=str, nargs='*', help="observation data")           
+    parser.add_argument("variable", type=str, help="model variable to process")
+    parser.add_argument("adjustment_file", type=str, help="adjustment factor file")
     parser.add_argument("output_file", type=str, help="output file")
 
+    parser.add_argument("--obs_units", type=str, default=None, help="obs data units")
+    parser.add_argument("--adjustment_units", type=str, default=None, help="adjustment data units")
+    parser.add_argument("--output_units", type=str, default=None, help="output data units")
     parser.add_argument(
-        "--lon_selection",
+        "--lon_chunking",
         type=int,
         nargs=2,
-        metavar=('START_INDEX', 'END_INDEX'),
-        default=None,
-        help="take a subset of the data using a longitude selection"
+        metavar=('CHUNK_NUMBER', 'TOTAL_CHUNKS'),
+        default=(1, 1),
+        help="subset data along longitude dimension (chunk numbers start at 1)"
     )
     parser.add_argument(
         "--time_bounds",
@@ -77,6 +114,5 @@ if __name__ == '__main__':
         required=True,
         help="observations time bounds in YYYY-MM-DD format"
     )
-
     args = parser.parse_args()
     main(args)
--- calc_adjustment.py
+++ calc_adjustment.py
@@ -4,38 +4,65 @@ import argparse
 
 import numpy as np
 import xarray as xr
+import xclim as xc
 from xclim import sdba
 import cmdline_provenance as cmdprov
 
 
+def read_data(infiles, var, time_bounds, input_units, output_units):
+    """Read and process a multi-file dataset."""
+
+    ds = xr.open_mfdataset(infiles)
+    try:
+        ds = ds.drop('height')
+    except ValueError:
+        pass
+    da = ds[var]
+    start_date, end_date = time_bounds
+    da_period = da.sel({'time': slice(start_date, end_date)})
+    da_period = da_period.chunk({'time': -1})
+    if input_units:
+        da_period.attrs['units'] = input_units
+    if output_units:
+        da_period = xc.units.convert_units_to(da_period, output_units)
+
+    return da_period
+     
+
 def main(args):
     """Run the program."""
     
-    ds_hist = xr.open_mfdataset(args.hist_files)
-    da_hist = ds_hist[args.model_variable]
-    start_hist, end_hist = args.hist_time_bounds
-    da_hist_period = da_hist.sel({'time': slice(start_hist, end_hist)})
-    da_hist_period = da_hist_period.chunk({'time': -1})
-    # TODO: check/fix units
+    da_hist = read_data(
+        args.hist_files,
+        args.variable,
+        args.hist_time_bounds,
+        args.input_units,
+        args.output_units
+    )
+    da_fut = read_data(
+        args.fut_files,
+        args.variable,
+        args.fut_time_bounds,
+        args.input_units,
+        args.output_units
+    )
 
-    ds_fut = xr.open_mfdataset(args.fut_files)
-    da_fut = ds_fut[args.model_variable]
-    start_fut, end_fut = args.fut_time_bounds
-    da_fut_period = da_fut.sel({'time': slice(start_fut, end_fut)})
-    da_fut_period = da_fut_period.chunk({'time': -1})
-    # TODO: check/fit units
-
+    mapping_methods = {'additive': '+', 'multiplicative': '*'}
     qm = sdba.EmpiricalQuantileMapping.train(
         da_fut,
         da_hist,
         nquantiles=100,
         group="time.month",
-        kind="+"
+        kind=mapping_methods[args.method]
     )
     qm.ds = qm.ds.assign_coords({'lat': da_fut['lat'], 'lon': da_fut['lon']}) #xclim strips lat/lon attributes
     qm.ds = qm.ds.transpose('quantiles', 'month', 'lat', 'lon')
 
     qm.ds.attrs['history'] = cmdprov.new_log()
+    qm.ds.attrs['base_period_start'] = args.hist_time_bounds[0]
+    qm.ds.attrs['base_period_end'] = args.hist_time_bounds[1]
+    qm.ds.attrs['future_period_start'] =args.fut_time_bounds[0]
+    qm.ds.attrs['future_period_end'] = args.fut_time_bounds[1]
     qm.ds.to_netcdf(args.output_file)
 
 
@@ -47,7 +74,7 @@ if __name__ == '__main__':
     )
                                      
     parser.add_argument(
-        "model_variable", type=str, help="model variable to process"
+        "variable", type=str, help="model variable to process"
     )
     parser.add_argument("output_file", type=str, help="output file")
 
@@ -87,6 +114,12 @@ if __name__ == '__main__':
         choices=('additive', 'multiplicative'),
         default='additive',
         help="scaling method",
+    )
+    parser.add_argument(
+        "--input_units", type=str, default=None, help="input data units"
+    )
+    parser.add_argument(
+        "--output_units", type=str, default=None, help="output data units"
     )
 
     args = parser.parse_args()
