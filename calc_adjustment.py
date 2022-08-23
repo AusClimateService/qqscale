31d5eb1f1c4b45a4f7e7a83531320c77b147162e
Damien Irving <irving.damien@gmail.com>
2022-08-23 14:57:02 +1000

    Some changes

--- calc_adjustment.py
+++ calc_adjustment.py
@@ -1,29 +1,58 @@
 """Command line program for calculating QQ-scaling adjustment factors."""
 
 import argparse
+import logging
 
+import git
 import numpy as np
 import xarray as xr
 import xclim as xc
 from xclim import sdba
 import cmdline_provenance as cmdprov
+import dask.diagnostics
+from dask.distributed import Client, LocalCluster, progress
 
-import spatial_selection
+
+def profiling_stats(rprof):
+    """Record profiling information."""
+
+    max_memory = np.max([result.mem for result in rprof.results])
+    max_cpus = np.max([result.cpu for result in rprof.results])
+
+    logging.debug(f'Peak memory usage: {max_memory}MB')
+    logging.debug(f'Peak CPU usage: {max_cpus}%')
+
+    
+def get_new_log():
+    """Generate command log for output file."""
+
+    try:
+        repo = git.Repo()
+        repo_url = repo.remotes[0].url.split(".git")[0]
+    except (git.exc.InvalidGitRepositoryError, NameError):
+        repo_url = None
+    new_log = cmdprov.new_log(
+        code_url=repo_url,
+    )
+
+    return new_log
 
 
 def read_data(
     infiles,
     var,
     time_bounds=None,
-    spatial_sel=None,
-    spatial_agg="none",
     input_units=None,
     output_units=None,
     lon_chunk_size=20,
 ):
-    """Read and process a multi-file dataset."""
+    """Read and process an input dataset."""
 
-    ds = xr.open_mfdataset(infiles)
+    if len(infiles) == 1:
+        ds = xr.open_dataset(infiles[0])
+    else:
+        ds = xr.open_mfdataset(infiles)
+
     try:
         ds = ds.drop('height')
     except ValueError:
@@ -32,17 +61,13 @@ def read_data(
     if time_bounds:
         start_date, end_date = time_bounds
         ds = ds.sel({'time': slice(start_date, end_date)})
-    ds = ds.chunk({'time': -1, 'lon': lon_chunk_size})
 
-    if spatial_sel is None:
-        pass
-    elif len(spatial_sel) == 4:
-        ds = spatial_selection.select_box_region(ds, spatial_sel, agg=spatial_agg)
-    elif len(spatial_sel) == 2:
-        ds = spatial_selection.select_point_region(ds, spatial_sel)
-    else:
-        msg = "spatial sel must be None, a box (list of 4 floats) or a point (list of 2 floats)"
-        raise ValueError(msg)
+    chunk_dict = {'time': -1}
+    if lon_chunk_size:
+        chunk_dict['lon'] = lon_chunk_size
+    ds = ds.chunk(chunk_dict)
+    logging.debug(f'Array size: {ds[var].shape}')
+    logging.debug(f'Chunk size: {ds[var].chunksizes}')
 
     if input_units:
         ds[var].attrs['units'] = input_units
@@ -54,6 +79,15 @@ def read_data(
 
 def main(args):
     """Run the program."""
+    
+    if args.local_cluster:
+        assert args.dask_dir, "Must provide --dask_dir for local cluster"
+        dask.config.set(temporary_directory=args.dask_dir)
+        cluster = LocalCluster(n_workers=args.nworkers)
+        client = Client(cluster)
+        print("Watch progress at http://localhost:8787/status")
+    else:
+        dask.diagnostics.ProgressBar().register()
     
     ds_hist = read_data(
         args.hist_files,
@@ -80,10 +114,14 @@ def main(args):
         group="time.month",
         kind=mapping_methods[args.method]
     )
+    if args.local_cluster:
+        qm = qm.persist()
+        progress(qm)
+    
     qm.ds = qm.ds.assign_coords({'lat': ds_fut['lat'], 'lon': ds_fut['lon']}) #xclim strips lat/lon attributes
     qm.ds = qm.ds.transpose('quantiles', 'month', 'lat', 'lon')
 
-    qm.ds.attrs['history'] = cmdprov.new_log()
+    qm.ds.attrs['history'] = get_new_log()
     qm.ds.attrs['base_period_start'] = args.hist_time_bounds[0]
     qm.ds.attrs['base_period_end'] = args.hist_time_bounds[1]
     qm.ds.attrs['future_period_start'] =args.fut_time_bounds[0]
@@ -96,13 +134,9 @@ if __name__ == '__main__':
         description=__doc__,
         argument_default=argparse.SUPPRESS,
         formatter_class=argparse.RawDescriptionHelpFormatter
-    )
-                                     
-    parser.add_argument(
-        "variable", type=str, help="model variable to process"
-    )
+    )          
+    parser.add_argument("variable", type=str, help="model variable to process")
     parser.add_argument("output_file", type=str, help="output file")
-
     parser.add_argument(
         "--hist_files",
         type=str,
@@ -141,14 +175,50 @@ if __name__ == '__main__':
         help="scaling method",
     )
     parser.add_argument(
-        "--input_units", type=str, default=None, help="input data units"
+        "--input_units",
+        type=str,
+        default=None,
+        help="input data units"
     )
     parser.add_argument(
-        "--output_units", type=str, default=None, help="output data units"
+        "--output_units",
+        type=str,
+        default=None,
+        help="output data units"
     )
     parser.add_argument(
-        "--lon_chunk_size", type=int, default=-1, help="chunk size along the longitude axis"
+        "--verbose",
+        action="store_true",
+        default=False,
+        help='Set logging level to DEBUG',
     )
-
+    parser.add_argument(
+        "--local_cluster",
+        action="store_true",
+        default=False,
+        help='Use a local dask cluster',
+    )
+    parser.add_argument(
+        "--dask_dir",
+        type=str,
+        default=None,
+        help='Directory where dask worker space files can be written. Required for local cluster.',
+    )
+    parser.add_argument(
+        "--nworkers",
+        type=int,
+        default=None,
+        help='Number of workers for cluster',
+    )
+    parser.add_argument(
+        "--lon_chunk_size",
+        type=int,
+        default=None,
+        help='Size of longitude chunks (i.e. number of lons in each chunk)',
+    )
     args = parser.parse_args()
-    main(args)
+    log_level = logging.DEBUG if args.verbose else logging.WARNING
+    logging.basicConfig(level=log_level)
+    with dask.diagnostics.ResourceProfiler() as rprof:
+        main(args)
+    profiling_stats(rprof)
