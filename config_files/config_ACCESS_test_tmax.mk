4c850596c874d64fd45ef7a0db2487daa72c19e2
Damien Irving <irving.damien@gmail.com>
2022-08-26 16:32:40 +1000

    Small fix

--- config_files/config_ACCESS_test_tmax.mk
+++ config_files/config_ACCESS_test_tmax.mk
@@ -19,7 +19,7 @@ HIST_FILES := /g/data/fs38/publications/CMIP6/CMIP/CSIRO/ACCESS-ESM1-5/historica
 
 FUTURE_FILES := /g/data/fs38/publications/CMIP6/ScenarioMIP/CSIRO/ACCESS-ESM1-5/ssp370/r1i1p1f1/day/tasmax/gn/latest/tasmax_day_ACCESS-ESM1-5_ssp370_r1i1p1f1_gn_20150101-20641231.nc /g/data/fs38/publications/CMIP6/ScenarioMIP/CSIRO/ACCESS-ESM1-5/ssp370/r1i1p1f1/day/tasmax/gn/latest/tasmax_day_ACCESS-ESM1-5_ssp370_r1i1p1f1_gn_20650101-21001231.nc
 
-OBS_FILES := /g/data/xv83/agcd-csiro/tmax/daily/tmax_AGCD-CSIRO_r005_19100101-20220405_daily_spatial-chunking.zarr/
+OBS_FILES := /g/data/xv83/agcd-csiro/tmax/daily/tmax_AGCD-CSIRO_r005_19100101-20220405_daily_spatial-chunking.zarr
 
 
 
