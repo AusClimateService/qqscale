901e0e22332c5df47a230b2a1877c3e97da256c5
Damien Irving <irving.damien@gmail.com>
2023-02-20 16:08:12 +1100

    Add qmba validation template

--- example_workflows/make_qdc_post-processing.mk
+++ example_workflows/make_qdc_post-processing.mk
@@ -8,8 +8,6 @@ include ${CONFIG}
 PYTHON=/g/data/xv83/dbi599/miniconda3/envs/qqscale/bin/python
 PAPERMILL=/g/data/xv83/dbi599/miniconda3/envs/qqscale/bin/papermill
 CODE_DIR=/g/data/wp00/shared_code/qqscale
-VALIDATION_NOTEBOOK=${CODE_DIR}/example_validation/${QQ_BASE}.ipynb
-TEMPLATE_NOTEBOOK=${CODE_DIR}/example_validation/validation.ipynb
 
 ## cih-metadata: Apply CIH metadata to the QQ-scaled projection data
 cih-metadata : ${QQ_PATH}
@@ -23,11 +21,6 @@ ${QQ_PATH} :
 match-mean : ${QQ_MEAN_MATCH_PATH}
 ${QQ_MEAN_MATCH_PATH} : ${QQ_PATH}
 	${PYTHON} ${CODE_DIR}/match_mean_change.py $< ${TARGET_VAR} $@ --output_units ${OUTPUT_UNITS} --hist_files ${HIST_DATA} --hist_var ${HIST_VAR} --input_hist_units ${HIST_UNITS} --ref_files ${REF_DATA} --ref_var ${REF_VAR} --input_ref_units ${REF_UNITS} --target_files ${TARGET_DATA} --input_target_units ${TARGET_UNITS} --hist_time_bounds ${HIST_START}-01-01 ${HIST_END}-12-31 --ref_time_bounds ${REF_START}-01-01 ${REF_END}-12-31 --target_time_bounds ${TARGET_START}-01-01 ${TARGET_END}-12-31 --scaling ${SCALING} --verbose
-
-## validation : Create validation plots for QQ-scaled climate projection data
-validation : ${VALIDATION_NOTEBOOK}
-${VALIDATION_NOTEBOOK} : ${TEMPLATE_NOTEBOOK} ${AF_PATH} ${QQ_PATH} ${TARGET_Q_PATH}
-	${PAPERMILL} -p adjustment_file $(word 2,$^) -p qq_file $(word 3,$^) -r hist_files "${HIST_DATA}" -r ref_files "${REF_DATA}" -r target_files "${TARGET_DATA}" -r target_q_file $(word 4,$^) -r hist_time_bounds "${HIST_START}-01-01 ${HIST_END}-12-31" -r ref_time_bounds "${REF_START}-01-01 ${REF_END}-12-31" -r target_time_bounds "${TARGET_START}-01-01 ${TARGET_END}-12-31" -p example_lat ${EXAMPLE_LAT} -p example_lon ${EXAMPLE_LON} -p example_month ${EXAMPLE_MONTH} -p hist_units ${HIST_UNITS} -p ref_units ${REF_UNITS} -p target_units ${TARGET_UNITS} -p output_units ${OUTPUT_UNITS} -p hist_var ${HIST_VAR} -p ref_var ${REF_VAR} -p target_var ${TARGET_VAR} $< $@
 
 ## help : show this message
 help :
